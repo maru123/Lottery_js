03e87c0141d290e3fa2cad077e519b4705dd53eb
Itsumi Maruyama <maruyama.itsumi@b-basics.co.jp>
2015-06-12 11:50:30 +0900

    First Commit

---
+++ lottery.js
@@ -0,0 +1,54 @@
+
+    var Lottery = function(size){
+        this.size = size;
+        this.membersCount = 0;
+        this.members = [];
+        this.winners = [];
+    };
+    Lottery.prototype.add = function(name){
+        this.members.push(name);
+        return true;
+    };
+    Lottery.prototype.isMembers = function(name){
+        name = name.trim();
+        if (name.length === 0){
+            return false;
+        };
+        for (var i = 0; i < this.membersCount; i++) {
+            if (this.members[i] === name){
+                return false;
+            };
+        };
+        return true;
+    };
+    Lottery.prototype.getMembers = function(){
+        this.membersCount = this.members.length;
+    };
+    Lottery.prototype.getWinners = function(){
+        var m = this.members.concat();
+        var n = this.size;
+        var winnerIndex;
+        var winner;
+
+        while ( n===0,n--){
+            winnerIndex = Math.floor( Math.random() * this.members.length )
+            winner = m.splice(winnerIndex,1);
+            this.winners.push(winner[0]);
+        };
+    };
+    Lottery.prototype.AllWinners = function(){
+        this.winners = this.members.concat();
+    };
+    Lottery.prototype.resetWinners = function(){
+        this.winners = [];
+    };
+    Lottery.prototype.isSize = function(){
+        if(this.size <= 0){
+            return false;
+        };
+    };
+    Lottery.prototype.isMembersCunt = function(){
+        if(this.membersCount === 0 ){
+            return false;
+        };
+    };
