8b08bc114710dcfe55b5af393b0cfe623dc83af4
Itsumi Maruyama <maruyama.itsumi@b-basics.co.jp>
2015-06-12 23:26:03 +0900

    add weight etc

--- lottery.js
+++ lottery.js
@@ -3,11 +3,15 @@
         this.prizeCount = prizeCount;
         this.members = [];
         this.winners = [];
+        this.lastMembers = [];
     };
-    Lottery.prototype.add = function(name){
+    Lottery.prototype.add = function(name,weight){
         if (this.isNotName(name)) return false;
         if (this.isNotNewMember(name)) return false;
-        this.members.push(name);
+        if (this.isNotPositiveNumber(weight)) return false;
+        for (var i = 1 ; i <= weight ;i++) {
+            this.members.push(name);
+        }
         return true;
     };
     Lottery.prototype.isNotName = function(name){
@@ -22,19 +26,27 @@
         }
         return false;
     };
+    Lottery.prototype.isNotPositiveNumber = function(weight){
+        if (weight < 0) {
+            return true;
+        }
+        return false;
+    };
     Lottery.prototype.getWinners = function(){
         this.winners = [];
         if (this.prizeCount >= this.members.length) return this.members;
-        var members = this.members.concat();
+        this.lastMembers = this.members.concat();
         var winnersCount = this.prizeCount;
 
         while ( winnersCount===0,winnersCount--){
-            var winnerIndex = Math.floor( Math.random() * members.length );
-            var winner = members.splice(winnerIndex,1);
-            this.winners.push(winner[0]);
-
-            //var removeWinner = winner[0]
-            //var removedWinner = members.filter(function(element){return element !== removeWinner});
-        };
+            var winnerIndex = Math.floor( Math.random() * this.lastMembers.length );
+            var winner = this.lastMembers[winnerIndex];
+            this.winners.push(winner);
+            this.remove(this.lastMembers,winner);
+        }
         return this.winners;
+    };
+    Lottery.prototype.remove = function (members,winner) {
+        var lastMembers = members.filter(function(element){return element !== winner});
+        this.lastMembers = lastMembers.concat();
     };
